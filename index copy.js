5e274c5ba86e81597fa7c606ed02c92396c27f69
Dominic Nguyen <dominicnguyen@huskers.unl.edu>
2017-10-21 19:21:21 -0500

    Removed undo function

--- index copy.js
+++ index copy.js
@@ -1,13 +1,11 @@
 var arrObjects = [];
 var arrPlaced = [];
+var allObjectsPlaced = [];
+var elements = [];
+var boxesModified = [];
+var moves = [];
 var lastPlaced;
-var currBox;
-var userWin = false;
-
-// Assign HTML elements into variables 
-var checkBtn = document.getElementById("checkBtn");
-var resetBtn = document.getElementById("resetBtn");
-var messageDiv = document.getElementById("messageDiv");
+var lastBox;
 
 //Box Constructor
 function boxObject(id, type, x, y) {
@@ -124,7 +122,7 @@ var inputSideBoxID;
 var outputSideBoxID;
 
 // Function to generate random box location
-function randomLocation() {
+function randomGenerator() {
     var arrayLetters = ["T", "L", "B", "R"];
     var ranLetter = parseInt(Math.random()*arrayLetters.length);
     var ranNumber = parseInt(Math.random()*Math.sqrt(arrObjects.length));
@@ -162,31 +160,13 @@ function randomLocation() {
     return arrayLetters[ranLetter] + (ranNumber + 1).toString();
 }
 
-var inputPressure;
-var inputTemperature;
-var outputPressure;
-var outputTemperature;
-var pressureDrop = 1;
-var tempInc = 50;
-
-function randomProcessCond() {
-    inputPressure = randInt(20, 30);
-    inputTemperature = randInt(1, 4) * 50;
-    outputPressure = inputPressure - randInt(5, 10);
-    outputTemperature = inputTemperature + randInt(0, 2) * 50;
-}
-
-function randInt(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
 // Function to generate random input and output and highlight starting box
 function initiate() {
-    inputSideBoxID = randomLocation();
+    inputSideBoxID = randomGenerator();
     input = temp;
     input.highlight = true;
     while (input == temp) {
-        outputSideBoxID = randomLocation();
+        outputSideBoxID = randomGenerator();
     }
     output = temp;
     console.log("Input Box ID: " + input.id + 
@@ -195,197 +175,194 @@ function initiate() {
                 "\nOutput Side Box ID: " + outputSideBoxID);
     
     highlightBoxes();
-    
-    randomProcessCond();
-
-    // Highlight and display input and output process conditions
-    var inputSideBox = document.getElementById(inputSideBoxID);
-    inputSideBox.classList.add("sideHighlighted");
-    inputSideBox.innerHTML = "<h2>Input<br>P: " + inputPressure + "<br>T: " + inputTemperature + "</h2>";
-    
-    var outputSideBox = document.getElementById(outputSideBoxID);
-    outputSideBox.classList.add("sideHighlighted");
-    outputSideBox.innerHTML = "<h2>Output<br>P: " + outputPressure + "<br>T: " + outputTemperature + "</h2>";
 }
 initiate();
+
+var inputPressure;
+var inputTemperature;
+var outputPressure;
+var outputTemperature;
+var pressureDrop = 1;
+var tempInc = 50;
 
 // Function to check if outlet process conditions are satisfied
 function checkSolution(arr) {
     var finalPressure = inputPressure;
     var finalTemperature = inputTemperature;
-    for (var i = 0; i < arrPlaced.length; i++) {
-        finalPressure += arrPlaced[i].pressure;
-        finalTemperature += arrPlaced[i].temperature;
+    for (var i = 0; i < arr.length; i++) {
+        finalPressure += arr[i].pressure;
+        finalTemperature += arr[i].temperature;
     }
-    
     if (finalPressure == outputPressure && finalTemperature == outputTemperature) {
-        
-        
-        messageDiv.innerHTML = "<br><h1 class='green'>You win!</h1><br>Final Conditions<br>P: " + 
-            finalPressure + "<br>T: " + finalTemperature + 
-            "<br><br>Required Conditions<br>P: " + outputPressure + "<br>T: " + outputTemperature;
-        openModal();
-        userWin = true;
-        
-    } else {
-        
-//        alert("Sorry, that's not quite right.\n\nRequired Conditions\nP: " + 
-//              outputPressure + "\nT: " + outputTemperature +
-//              "\n\nFinal Conditions\nP: " + finalPressure + "\nT: " + finalTemperature
-//             );
-        
-        messageDiv.innerHTML = "<br><h1 class='red'>Try again!</h1><br>Final Conditions<br>P: " + 
-            finalPressure + "<br>T: " + finalTemperature + 
-            "<br><br>Required Conditions<br>P: " + outputPressure + "<br>T: " + outputTemperature;
-        openModal();
-        
-        // Complete reset board
-        completeReset();
-        
-        // Hide checkBtn
-        checkBtn.style.display = "none";
-        
+        alert("You won!");
     }
-}
-
-// Function to completely reset game
-function completeReset() {
-    for (var i = 0; i < arrPlaced.length; i++) {
-            //TODO: redefine reset undo();
-        }
+    else {
+        alert("Sorry, that's not quite right.");
+    }
 }
 
 /* Drag and Drop Functions */
-var bluePipeNum = 1, greenPipeNum = 1, heaterNum = 1;
-var bluePipeContainer = document.getElementById("bluePipeContainer");
-var greenPipeContainer = document.getElementById("greenPipeContainer");
-var heaterContainer = document.getElementById("heaterContainer");
+var pipeNum = 1, valveNum = 1, heaterNum = 1;
+var pipeContainer = document.getElementById("pipe-container");
+var valveContainer = document.getElementById("valve-container");
+var heaterContainer = document.getElementById("heater-container");
 
 function allowDrop(ev) {
     ev.preventDefault();
-} 
+}
 function drag(ev) {
 	ev.dataTransfer.setData("text", ev.target.id);
 }
 
 var oldElement;
 var newElement;
+var currentBox;
 function drop(ev) {
-    currBox = ev.currentTarget;
-    console.log("Attempted to place element in Box " + currBox.id);
-    var boxArrayLoc = currBox.id - 1;
+    var box = ev.currentTarget;
+    console.log("Attempted to place element in Box " + box.id);
+    var boxArrayLoc = box.id - 1;
     
     // If placing invalid box
     if (arrObjects[boxArrayLoc].highlight == false && arrObjects[boxArrayLoc].type == "empty") {
         
-        console.log("%cFAILED: Box " + currBox.id + " is not highlighted", "color:red");
+        console.log("%cFAILED: Box " + box.id + " is not highlighted", "color:red");
         
     } else { // Placing in valid box
-        oldElement = currBox.childNodes[0];
+        
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
         ev.target.innerHTML="";
+        var id = ev.currentTarget.id;
+        var currentBox = document.getElementById(id);
         newElement = document.getElementById(data);
+        oldElement = currentBox.children[0];
         
-//        console.log(oldElement);
+        // Push box into "boxesModified" array and new element into "elements" array
+        boxesModified.push(box);
+        elements.push(newElement);
 
         // Replace an existing element with new element
-        if (currBox.hasChildNodes()) {
+        if(box.hasChildNodes()) {
+            eraseBox(arrObjects[box.id - 1]);
+//            for (var i = 0; i < arrPlaced.length; i++) {
+//                if (arrPlaced[i].id == box.id) {
+//                    var replacement = arrPlaced[i];
+//                }
+//            }
             
-            eraseBox(arrObjects[currBox.id - 1]);
-            
-            currBox.removeChild(currBox.childNodes[0]);
-            currBox.appendChild(newElement);
-            
-            // Transfer element properties to box object
-            transferProperties(currBox.id, newElement);
-            
-            console.log("%cSUCCESS: Replaced " + oldElement.id + " in Box " + 
-                        currBox.id + " with " + newElement.id, "color:green");
-            
-        } else { //No error, push value
+            box.removeChild(box.childNodes[0]);
+            box.appendChild(newElement);
+
+            if (newElement.id.startsWith("pipe")) {
+
+                    arrObjects[id - 1].type = newElement.id;
+                    arrObjects[id - 1].pressure = arrObjects[id - 1].pressure - pressureDrop;
+//                    replacement.type = newElement.id;
+//                    replacement.pressure = -pressureDrop;
                 
-            currBox.appendChild(newElement);
-            newElement = currBox.children[0];
-          
-            // Transfer element properties to box object
-            transferProperties(currBox.id, newElement);
-            
-            arrPlaced.push(arrObjects[currBox.id - 1]);
+            } else if (newElement.id.startsWith("heater")) {
+
+                    arrObjects[id - 1].type = newElement.id;
+                    arrObjects[id - 1].pressure = arrObjects[id - 1].pressure - pressureDrop;
+                    arrObjects[id - 1].temperature = arrObjects[id - 1].temperature + tempInc;
+//                    replacement.type = newElement.id;
+//                    replacement.pressure = -pressureDrop;
+//                    replacement.temperature = tempInc;
+                
+            } else if (newElement.id.startsWith("valve")) {
+
+                    arrObjects[id - 1].type = newElement.id;
+                    arrObjects[id - 1].pressure = -pressureDrop;
+//                    replacement.type = newElement.id;
+//                    replacement.pressure = -pressureDrop;
+                    //TODO
+            }
+            console.log("%cSUCCESS: Replaced " + oldElement.id + " in Box " + 
+                        box.id + " with " + newElement.id, "color:green");
+//                console.log(arrPlaced);
             lastPlaced = arrPlaced.length - 1;
             arrPlaced[lastPlaced].highlight = false;
             
-            console.log("%cSUCCESS: Placed " + newElement.id + 
-                        " in Box " + currBox.id, "color:green");
+            // Add move to "moves" array
+            moves.push("replace");
+            
+        } else { //No error, push value
+                
+            box.appendChild(newElement);
+            newElement = currentBox.children[0];
+            if (newElement.id.startsWith("pipe")) {
+                
+                    arrObjects[id - 1].type = newElement.id;
+                    arrObjects[id - 1].pressure = arrObjects[id - 1].pressure - pressureDrop;
+                
+            } else if (newElement.id.startsWith("heater")) {
+                
+                    arrObjects[id - 1].type = newElement.id;
+                    arrObjects[id - 1].pressure = arrObjects[id - 1].pressure - pressureDrop;
+                    arrObjects[id - 1].temperature = arrObjects[id - 1].temperature + tempInc;
+                
+            } else if (newElement.id.startsWith("valve")) {
+                
+                    arrObjects[id - 1].type = newElement.id;
+                    arrObjects[id - 1].pressure = arrObjects[id - 1].pressure - pressureDrop;
+                    //TODO
+            }
+            arrPlaced.push(arrObjects[id - 1]);
+            lastPlaced = arrPlaced.length - 1;
+            arrPlaced[lastPlaced].highlight = false;
+            
+            console.log("%cSUCCESS: Placed " + newElement.id + " in Box " + box.id, "color:green");
+            
+            moves.push("place");
         }
         
+        // Check if puzzle solved
+        if (box.id == output.id) {
+            checkSolution(arrPlaced);
+        }
+
         // Rehighlight boxes
-        if (currBox.id != output.id) {
-            
-            console.log("Output not reached");
-            rehighlight(arrPlaced[lastPlaced]);
-            
-        } else { // Remove all highlights
-            
-            resetHighlight();
-            highlightBoxes();
-            
-            // Make checkBtn visible
-            checkBtn.style.display = "flex";
-            console.log("Output reached");
-            
-        }
-        
+        rehighlight(arrPlaced[lastPlaced]);
+
         // Replace powerup container
         replacePowerUp();
         console.log(arrObjects);
-        
-        // Display undoBtn & resetBtn if there's at least 1 element on grid
-        if (arrPlaced.length == 1) {
-            resetBtn.style.display = "flex";
-        }
     }
-    
+    allObjectsPlaced.push(arrObjects[id - 1]);
 }
 
 var container = document.getElementById("powerup-bar");
 // Function to replace powerup
 function replacePowerUp() {
     
-    for(var i = 0; i < 3; i++) {
-        if(container.children[i].children[0] == undefined) {
-            
-            if (container.children[i].id.startsWith("bluePipe")) {
-                
-                bluePipeNum++;
-                bluePipeContainer.innerHTML = "<div id='bluePipe" + bluePipeNum + 
-                    "' class='bluePipe element' draggable='true' ondragstart='drag(event)'></div>";
-                
-            } else if (container.children[i].id.startsWith("greenPipe")) {
-                            
-                greenPipeNum++;
-                greenPipeContainer.innerHTML = "<div id='greenPipe" + greenPipeNum + 
-                    "' class='greenPipe element' draggable='true' ondragstart='drag(event)'></div>";
-            
-            } else if (container.children[i].id.startsWith("heater")) {
-                            
-                heaterNum++;
-                heaterContainer.innerHTML = "<div id='heater" + heaterNum + 
-                    "' class='heater element' draggable='true' ondragstart='drag(event)'><div>&#9832;</div></div>";
-            
-            }
-                    
+    for(var i = 0; i < 3; i++)
+        {
+            if(container.children[i].children[0] == undefined)
+                {
+                    if(container.children[i].id.startsWith("pipe"))
+                        {
+                            pipeNum++;
+                            pipeContainer.innerHTML = "<div id='pipe" + pipeNum + "' class='pipe element' draggable='true' ondragstart='drag(event)'></div>";
+                        }
+                    else if(container.children[i].id.startsWith("heater"))
+                        {
+                            heaterNum++;
+                            heaterContainer.innerHTML = "<div id='heater" + heaterNum + "' class='heater element' draggable='true' ondragstart='drag(event)'></div>";
+                        }
+                    else if(container.children[i].id.startsWith("valve"))
+                        {
+                            valveNum++;
+                            valveContainer.innerHTML = "<div id='valve" + valveNum + "' class='valve element' draggable='true' ondragstart='drag(event)'></div>";
+                        }
+                }
         }
-    }
 }
 
-// Function to set all boxes highlight property to false
 function resetHighlight() {
     for (var i = 0; i < arrObjects.length; i++) {
             arrObjects[i].highlight = false;
         }
 }
-
 // Function to visually show highlighted boxes
 function highlightBoxes() {
     // Look in objects array and boxes that have highlighted = true
@@ -405,44 +382,16 @@ function rehighlight(box) {
     resetHighlight();
 
     // Assign highlighted boxes
-//    console.log(box);
     checkNearby(box);
    
+
     // Visually highlight assigned boxes
     highlightBoxes();
 }
 
 // Function to transfer properties of element to box object
-function transferProperties(boxID, element) {
+function transferProperties(element) {
     
-    var boxObjectLoc = boxID - 1;
-    var currBoxObj = arrObjects[boxObjectLoc];
-    
-    
-    if (element.id.startsWith("bluePipe")) {
-
-        currBoxObj.type = element.id;
-        currBoxObj.pressure = -pressureDrop;
-        
-    } else if (element.id.startsWith("greenPipe")) {
-
-        // Green Pipe has 2x more pressure drop than Blue Pipe
-        currBoxObj.type = element.id;
-        currBoxObj.pressure = -pressureDrop * 2;
-
-    } else if (element.id.startsWith("heater")) {
-
-        currBoxObj.type = element.id;
-        currBoxObj.pressure = -pressureDrop;
-        currBoxObj.temperature = tempInc;
-
-    } else {
-        
-        currBoxObj.type = "empty";
-        currBoxObj.pressure = 0;
-        currBoxObj.temperature = 0;
-        
-    }
 }
 
 //Function to highlight last element placed to be removed
@@ -453,35 +402,41 @@ function highlightRemove() {
     highlightBoxes();
 }
 
-function resetGame() {
+function undo() {
+    
+    if (allObjectsPlaced.length != 0) {
+        
+        lastBox = boxesModified.pop();
+        var lastBoxObject = allObjectsPlaced.pop();
+        var prevBoxObject = allObjectsPlaced[allObjectsPlaced.length - 1];
+        var lastMove = moves.pop();
+        var lastElement = elements.pop();
+        var prevElement = elements[elements.length - 1];
+        
+        // Remove last element from last box
+        lastBox.removeChild(lastElement);
+        if (lastMove == "replace") {
+            lastBox.appendChild(prevElement);
+        }
+
+        // Rehighlight boxes
+        if (allObjectsPlaced.length == 0) {
+            totalReset();
+        } else {
+            rehighlight(prevBoxObject);
+        }
+    }
+    
+}
+
+function totalReset() {
     
     resetHighlight();
     input.highlight = true;
     highlightBoxes();
     arrPlaced = [];
+    allObjectsPlaced = [];
+    elements = [];
+    boxesModified = [];
     moves = [];
-    resetBtn.style.display = "none";
-}
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Open the modal 
-function openModal() {
-    modal.style.display = "block";
-}
-
-// Close the modal when the user clicks on the x btn
-span.onclick = function() {
-    modal.style.display = "none";
-    if (userWin) {
-        location.reload();
-    }
-    userWin = false;
 }
