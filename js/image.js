73219509974671faf338a6c5a3957885b60f262d
Dominic Nguyen <dominicnguyen@huskers.unl.edu>
2017-10-31 00:43:15 -0500

    Change heater connection UI
    - Added images of heaters for different connections and adjusted code accordingly.

--- js/image.js
+++ js/image.js
@@ -5,76 +5,62 @@ function getPlacedImgLink(objPlacedID) {
     var currType = (currBoxObj.type).split('-')[0]; // Get type of obj (w/o num)
     var dirPrev;
     var source;
-     
-    if (currType != 'heater') { // Element not a heater
-        
-        // Placed obj at input
-        if (arrPlaced[objPlacedID - 1] === undefined) { // obj is first obj
-            
-            var inputFirstLet = inputSideBoxID[0];
-            debugger;
-            if (inputFirstLet === "T") {
-                dirPrev = "FromTop";
-            } else if (inputFirstLet === "R") {
-                dirPrev = "FromRight";
-            } else if (inputFirstLet === "B") {
-                dirPrev = "FromBot";
-            } else {
-                dirPrev = "FromLeft";
-            }
-        } else { // Placed obj not at input
-            // TODO:
-            var prevBoxObj = arrPlaced[objPlacedID-1];
+      
+    // Placed obj at input
+    if (arrPlaced[objPlacedID - 1] === undefined) { // obj is first obj
 
-            prevX = prevBoxObj.xcoord;
-            prevY = prevBoxObj.ycoord;
-            var deltaX = currX - prevX;
-            var deltaY = currY - prevY;
-
-            // Determine direction (dir)
-            if (Math.abs(deltaX) === 1) { // new obj is left or right of prev obj
-                if (deltaX === 1) {// new obj is right of prev obj
-                    dirPrev = "FromLeft";
-                } else { // ew obj is left of prev obj
-                    dirPrev = "FromRight";
-                }
-            } else if (Math.abs(deltaY) === 1) { // new obj is above or below prev obj
-                if (deltaY === 1) {// new obj is above of prev obj
-                    dirPrev = "FromBot";
-                } else { // new obj is below of prev obj
-                    dirPrev = "FromTop";
-                }
-            }
-
-            // Get link of replace img in prev obj and replace img
-            var prevBoxID = arrPlaced[objPlacedID - 1].id;
-            var prevBox = document.getElementById(prevBoxID);
-            debugger;
-            addBoxImg(prevBox, getReplacedImgLink(objPlacedID - 1));
-
-        }
-        
-        if (dirPrev === "FromTop" || dirPrev === "FromBot") {
-            dirPrev = "Vertical";
-        } else if (dirPrev === "FromLeft" || dirPrev === "FromRight") {
-            dirPrev = "Horizontal";
-        }
-        
-        source = 'res/' + currType + dirPrev + '.png';
+        var inputFirstLet = inputSideBoxID[0];
         debugger;
-        return source;
-        
-    } else { // element is heater
-        
-        // Temporary patch to fix placing heater connection bug
-        if (arrPlaced[objPlacedID - 1] !== undefined) {
-            var prevBoxID = arrPlaced[objPlacedID - 1].id;
-            var prevBox = document.getElementById(prevBoxID);
-            addBoxImg(prevBox, getReplacedImgLink(objPlacedID - 1));
+        if (inputFirstLet === "T") {
+            dirPrev = "FromTop";
+        } else if (inputFirstLet === "R") {
+            dirPrev = "FromRight";
+        } else if (inputFirstLet === "B") {
+            dirPrev = "FromBot";
+        } else {
+            dirPrev = "FromLeft";
         }
-        return 'res/heater.png';
+    } else { // Placed obj not at input
+        var prevBoxObj = arrPlaced[objPlacedID-1];
+
+        prevX = prevBoxObj.xcoord;
+        prevY = prevBoxObj.ycoord;
+        var deltaX = currX - prevX;
+        var deltaY = currY - prevY;
+
+        // Determine direction (dir)
+        if (Math.abs(deltaX) === 1) { // new obj is left or right of prev obj
+            if (deltaX === 1) {// new obj is right of prev obj
+                dirPrev = "FromLeft";
+            } else { // ew obj is left of prev obj
+                dirPrev = "FromRight";
+            }
+        } else if (Math.abs(deltaY) === 1) { // new obj is above or below prev obj
+            if (deltaY === 1) {// new obj is above of prev obj
+                dirPrev = "FromBot";
+            } else { // new obj is below of prev obj
+                dirPrev = "FromTop";
+            }
+        }
+
+        // Get link of replace img in prev obj and replace img
+        var prevBoxID = arrPlaced[objPlacedID - 1].id;
+        var prevBox = document.getElementById(prevBoxID);
+        debugger;
+        addBoxImg(prevBox, getReplacedImgLink(objPlacedID - 1));
+
     }
-    
+
+    if (dirPrev === "FromTop" || dirPrev === "FromBot") {
+        dirPrev = "Vertical";
+    } else if (dirPrev === "FromLeft" || dirPrev === "FromRight") {
+        dirPrev = "Horizontal";
+    }
+
+    source = 'res/' + currType + dirPrev + '.png';
+    debugger;
+    return source;
+
 }
 
 function getReplacedImgLink(objPlacedID) {
@@ -85,121 +71,110 @@ function getReplacedImgLink(objPlacedID) {
     var dirPrev;
     var dirNext = '';
     var source;
-     
-    if (currType != 'heater') { // Element not a heater
-        
-        // Replaced obj at input
-        if (arrPlaced[objPlacedID - 1] === undefined) { // obj is first obj
-            
-            var inputFirstLet = inputSideBoxID[0];
-            debugger;
-            if (inputFirstLet === "T") {
-                dirPrev = "FromTop";
-            } else if (inputFirstLet === "R") {
-                dirPrev = "FromRight";
-            } else if (inputFirstLet === "B") {
-                dirPrev = "FromBot";
-            } else {
+    
+    // Replaced obj at input
+    if (arrPlaced[objPlacedID - 1] === undefined) { // obj is first obj
+
+        var inputFirstLet = inputSideBoxID[0];
+        debugger;
+        if (inputFirstLet === "T") {
+            dirPrev = "FromTop";
+        } else if (inputFirstLet === "R") {
+            dirPrev = "FromRight";
+        } else if (inputFirstLet === "B") {
+            dirPrev = "FromBot";
+        } else {
+            dirPrev = "FromLeft";
+        }
+        console.log("Replacing at input");
+    } else { // Replaced obj not at input
+        // TODO:
+        var prevBox = arrPlaced[objPlacedID-1];
+
+        prevX = prevBox.xcoord;
+        prevY = prevBox.ycoord;
+        var deltaX = currX - prevX;
+        var deltaY = currY - prevY;
+
+        // Determine previous direction (dirPrev)
+        if (Math.abs(deltaX) === 1) { // new obj is left or right of prev obj
+            if (deltaX === 1) {// new obj is right of prev obj
                 dirPrev = "FromLeft";
+            } else { // ew obj is left of prev obj
+                dirPrev = "FromRight";
             }
-            console.log("Replacing at input");
-        } else { // Replaced obj not at input
-            // TODO:
-            var prevBox = arrPlaced[objPlacedID-1];
-
-            prevX = prevBox.xcoord;
-            prevY = prevBox.ycoord;
-            var deltaX = currX - prevX;
-            var deltaY = currY - prevY;
-
-            // Determine previous direction (dirPrev)
-            if (Math.abs(deltaX) === 1) { // new obj is left or right of prev obj
-                if (deltaX === 1) {// new obj is right of prev obj
-                    dirPrev = "FromLeft";
-                } else { // ew obj is left of prev obj
-                    dirPrev = "FromRight";
-                }
-            } else if (Math.abs(deltaY) === 1) { // new obj is above or below prev obj
-                if (deltaY === 1) {// replaced obj is below new obj
-                    dirPrev = "FromTop";
-                } else { // replaced obj is above new obj
-                    dirPrev = "FromBot";
-                }
+        } else if (Math.abs(deltaY) === 1) { // new obj is above or below prev obj
+            if (deltaY === 1) {// replaced obj is below new obj
+                dirPrev = "FromTop";
+            } else { // replaced obj is above new obj
+                dirPrev = "FromBot";
             }
         }
-        
-        // Add additional direction for replace object that has two connections
-        var nextBox = arrPlaced[objPlacedID + 1];
-        if (nextBox !== undefined) {
-            nextX = nextBox.xcoord;
-            nextY = nextBox.ycoord;
-            var deltaX = nextX - currX;
-            var deltaY = nextY - currY;
-                
-            // Determine next direction (direNext)
-            if (Math.abs(deltaX) === 1) { // new obj is left or right of next obj
-                if (deltaX === 1) {// replaced obj is left of new obj
-                    dirNext = "AndRight";
-                } else { // replaced obj is right of new obj
-                    dirNext = "AndLeft";
-                }
-            } else if (Math.abs(deltaY) === 1) { // new obj is above or below next obj
-                if (deltaY === 1) {// replaced obj is above new obj
-                    dirNext = "AndBot";
-                } else { // replaced obj is below new obj
-                    dirNext = "AndTop";
-                }
-            }
-        } 
-        else if (currBox.id === output.id) { // If replacing or placing at last box
-            var outputFirstLet = outputSideBoxID[0];
-            if (outputFirstLet === "T") {
-                dirNext = "AndTop";
-            } else if (outputFirstLet === "R") {
+    }
+
+    // Add additional direction for replace object that has two connections
+    var nextBox = arrPlaced[objPlacedID + 1];
+    if (nextBox !== undefined) {
+        nextX = nextBox.xcoord;
+        nextY = nextBox.ycoord;
+        var deltaX = nextX - currX;
+        var deltaY = nextY - currY;
+
+        // Determine next direction (direNext)
+        if (Math.abs(deltaX) === 1) { // new obj is left or right of next obj
+            if (deltaX === 1) {// replaced obj is left of new obj
                 dirNext = "AndRight";
-            } else if (outputFirstLet === "B") {
-                dirNext = "AndBot";
-            } else {
+            } else { // replaced obj is right of new obj
                 dirNext = "AndLeft";
             }
-            
-            // Calls replace on prev box
-            var prevBoxID = arrPlaced[objPlacedID - 1].id;
-            var prevBox = document.getElementById(prevBoxID);
-            addBoxImg(prevBox, getReplacedImgLink(objPlacedID - 1));
+        } else if (Math.abs(deltaY) === 1) { // new obj is above or below next obj
+            if (deltaY === 1) {// replaced obj is above new obj
+                dirNext = "AndBot";
+            } else { // replaced obj is below new obj
+                dirNext = "AndTop";
+            }
         }
-        
-        
-        var totalDir = dirPrev + dirNext;
-        if (totalDir === "FromBotAndTop" || totalDir === "FromTopAndBot" || totalDir === "FromTop" || totalDir === "FromBot") {
-            totalDir = "Vertical";
-        } else if (totalDir === "FromLeftAndRight" || totalDir === "FromRightAndLeft" || totalDir === "FromLeft" || totalDir === "FromRight"){
-            totalDir = "Horizontal";
-        } else if (totalDir === "FromBotAndRight" || totalDir === "FromRightAndBot") {
-            totalDir = "SECorner";
-        } else if (totalDir === "FromTopAndRight" || totalDir === "FromRightAndTop") {
-            totalDir = "NECorner";
-        } else if (totalDir === "FromBotAndLeft" || totalDir === "FromLeftAndBot") {
-            totalDir = "SWCorner";
-        } else if (totalDir === "FromTopAndLeft" || totalDir === "FromLeftAndTop") {
-            totalDir = "NWCorner";
-        } 
-        
-        source = 'res/' + currType + totalDir + '.png';
-        
-        debugger;
-        return source;
-        
-    } else { // element is heater
-        // Temporary patch to fix placing heater connection bug
-        if (arrPlaced[objPlacedID - 1] !== undefined) {
-            var prevBoxID = arrPlaced[objPlacedID - 1].id;
-            var prevBox = document.getElementById(prevBoxID);
-            addBoxImg(prevBox, getReplacedImgLink(objPlacedID - 1));
+    } 
+    else if (currBox.id === output.id) { // If replacing or placing at last box
+        var outputFirstLet = outputSideBoxID[0];
+        if (outputFirstLet === "T") {
+            dirNext = "AndTop";
+        } else if (outputFirstLet === "R") {
+            dirNext = "AndRight";
+        } else if (outputFirstLet === "B") {
+            dirNext = "AndBot";
+        } else {
+            dirNext = "AndLeft";
         }
-        return 'res/heater.png';
+
+        // Calls replace on prev box
+        var prevBoxID = arrPlaced[objPlacedID - 1].id;
+        var prevBox = document.getElementById(prevBoxID);
+        addBoxImg(prevBox, getReplacedImgLink(objPlacedID - 1));
     }
-}
+
+
+    var totalDir = dirPrev + dirNext;
+    if (totalDir === "FromBotAndTop" || totalDir === "FromTopAndBot" || totalDir === "FromTop" || totalDir === "FromBot") {
+        totalDir = "Vertical";
+    } else if (totalDir === "FromLeftAndRight" || totalDir === "FromRightAndLeft" || totalDir === "FromLeft" || totalDir === "FromRight"){
+        totalDir = "Horizontal";
+    } else if (totalDir === "FromBotAndRight" || totalDir === "FromRightAndBot") {
+        totalDir = "SECorner";
+    } else if (totalDir === "FromTopAndRight" || totalDir === "FromRightAndTop") {
+        totalDir = "NECorner";
+    } else if (totalDir === "FromBotAndLeft" || totalDir === "FromLeftAndBot") {
+        totalDir = "SWCorner";
+    } else if (totalDir === "FromTopAndLeft" || totalDir === "FromLeftAndTop") {
+        totalDir = "NWCorner";
+    } 
+
+    source = 'res/' + currType + totalDir + '.png';
+
+    debugger;
+    return source;
+
+} 
 
 function addBoxImg(currBox, source) {
     debugger;
